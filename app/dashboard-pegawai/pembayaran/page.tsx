06a0aeb96033c774d2551660946505ece46ced95
Nur Febriana Bakri <nurfebrianab011@gmail.com>
2025-11-22 15:44:23 +0800

    modif transaksi dan pembayran di dashboard  pegawai

--- app/dashboard-pegawai/pembayaran/page.tsx
+++ app/dashboard-pegawai/pembayaran/page.tsx
@@ -1,11 +1,34 @@
-// app/promo/page.tsx
-import React from 'react';
+'use client';
 
-export default function Promo() {
-    return (
-        <div>
-            <h1>Pembayaran</h1>
-            <p>Halaman untuk Pembayaran .</p>
-        </div>
-    );
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './pembayaran.module.css';
+
+export default function PembayaranPage() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Pembayaran</h1>
+
+      <p className={styles.subText}>Belum ada pembayaran</p>
+      <p className={styles.descText}>
+        Silakan mengisi produk di daftar belanja pada halaman transaksi untuk menghitung harga yang perlu dibayar
+      </p>
+
+      <div className={styles.imageWrapper}>
+        <Image
+          src="/images/pembayaran.png"
+          alt="Ilustrasi Pembayaran"
+          width={650}
+          height={550}
+          className={styles.image}
+        />
+      </div>
+
+      <div className={styles.centerButton}>
+        <Link href="/dashboard-pegawai/transaksi">
+          <button className={styles.goTransaksiBtn}>Ke halaman transaksi</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
